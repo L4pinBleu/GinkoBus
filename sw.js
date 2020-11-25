2830cbd45b44286a373a75d608a5705f946ea6e7
nmor <nicolas.mor@edu.univ-fcomte.fr>
2020-11-25 15:11:21 +0100

    Add service worker

---
+++ sw.js
@@ -0,0 +1,45 @@
+/**
+  * Service Worker
+  */
+var cacheName      = 'bigFatBus';
+
+// Var is used if new cache need to be set.
+var contentToCache = appShellFiles;
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(contentToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+    console.log('[Service Worker] Fetched resource '+ e.request.url);
+    e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+        if(key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
